bb8f083766d6c9e4be17e8b36023382d89b088aa
cewerling <cwerling@cornerstones.com>
2020-04-07 23:50:43 -0400

    end of day

--- 05-Node-Server/server/app.js
+++ 05-Node-Server/server/app.js
@@ -1,16 +1,34 @@
+require('dotenv').config();
+
 var express = require('express');
 var app = express();
-var test = require('./controllers/testcontroller')
+var test = require('./controllers/testcontroller');
+var authTest = require('./controllers/authtestcontroller');
+var user = require('./controllers/usercontroller');
 var sequelize = require('./db');
 
 sequelize.sync();  // tip: pass in {force: true} for resetting tables
+// express.json();  // one place had this as needed, but looks like a duplicate of the line below, which this place did not have "app.use(express.json());"
+// place: https://elevenfifty.instructure.com/courses/187/pages/changes-to-app-dot-js?module_item_id=13387
+app.use(express.json());
+app.use(require('./middleware/headers'));
 
-app.use('/test', test)
+/********************
+ * EXPOSED ROUTES
+ ********************/
+app.use('/test', test);
+app.use('/api/user', user);
+
+/*********************
+ * PROTECTED ROUTES
+ *********************/
+app.use(require('./middleware/validate-session'));
+app.use('/authtest', authTest);
 
 app.listen(3000, function() {
-    console.log('Hey man!!!');
+    console.log('App is listening on 3000.');
 });
 
-app.use('/api/test', function(req, res) {
+app.use('/api/test', function(req, res) {  // This isn't in later modules
     res.send("This is data from the /api/test endpoint.  It's from the server.");
 });
--- 05-Node-Server/server/controllers/testcontroller.js
+++ 05-Node-Server/server/controllers/testcontroller.js
@@ -13,7 +13,7 @@ var TestModel = sequelize.import('../models/test');
 
 
 /****************************************
- * Controller Method #1: Simple Response
+ * Controller Method #2: Persisting Data
 ****************************************/
 
 router.post('/two', function(req, res) {
@@ -26,6 +26,138 @@ router.post('/two', function(req, res) {
           res.send("Test two went through!")
       })
 });
+
+/****************************************
+ * Controller Method #3: req.body
+****************************************/
+
+router.post('/three', function(req, res) {
+    let testData = req.body.testdata.item;
+
+    TestModel
+      .create({
+          testdata: testData
+      })
+    res.send("Test three went through!");
+    console.log("Test three went through!");
+});
+
+router.post('/four', function(req, res) {
+    let testData = req.body.testdata.item;
+    TestModel
+      .create({
+          testdata: testData
+      })
+      .then(
+        function message() {
+           res.send("Test 4 went through!");
+        }
+      );
+});
+
+
+/****************************************
+ * Route 5: Return data in a Promise
+****************************************/
+
+router.post('/five', function(req, res) {
+    let testData = req.body.testdata.item;
+
+    TestModel
+      .create({
+          testdata: testData
+      })
+      .then(
+        function message(data) {
+           res.send(data);
+        }
+      );
+});
+
+
+
+
+
+/****************************************
+ * Route 6: Return data as JSON
+****************************************/
+
+router.post('/six', function(req, res) {
+    let testData = req.body.testdata.item;
+
+    TestModel
+      .create({
+          testdata: testData
+      })
+      .then(
+        function message(testdata) {
+           res.json( {
+               testdata: testdata
+           });
+        }
+      );
+});
+
+
+
+/****************************************
+ * Route 7: Handle errors
+****************************************/
+
+router.post('/seven', function(req, res) {
+    let testData = req.body.testdata.item;
+
+    TestModel
+      .create({
+          testdata: testData
+      })
+      .then(
+        function createSuccess(testdata) {
+           res.json( {
+               testdata: testdata
+           });
+        },
+        function createError(err) {
+            res.send(500, err.message);
+        }
+      );
+});
+
+
+
+/****************************************
+ * GET: Get simple message from server
+****************************************/
+
+router.get('/helloclient', function(req, res) {
+    res.send("This is a message from the server to the client.")
+})
+
+
+
+
+/****************************************
+ * GET: /one
+****************************************/
+
+router.get('/one', function(req, res) {
+
+    TestModel
+      .findAll ({
+          attributes: ['id', 'testdata']
+      })
+      .then (
+          function findAllSuccess(data) {
+              console.log("Controller data:", data);
+              res.json(data);
+          },
+          function finaAllError(err) {
+              res.send(500, erro.message);
+          }
+      );
+});
+
+
 
 module.exports = router;
 
