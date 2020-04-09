0a99480c83eb7ca32b317963b9d679651b135937
cewerling <cwerling@cornerstones.com>
2020-04-09 00:17:56 -0400

    end of day

--- 05-Node-Server/server/middleware/validate-session.js
+++ 05-Node-Server/server/middleware/validate-session.js
@@ -7,20 +7,28 @@ module.exports = function(req, res, next) {
         next()
     } else {
         var sessionToken = req.headers.authorization;
-        console.log(sessionToken)
+        console.log('');
+        console.log('');
+        console.log('******** Headers = ' + JSON.stringify(req.headers) + '*********');
+        console.log('******** Content-Type = ' + JSON.stringify(req.headers['content-type']) + '*********');
+        console.log('******** Authorization = ' + JSON.stringify(req.headers['authorization']) + '*********');
+        console.log('******** Session Token = ' + sessionToken + '*********');
         if (!sessionToken) return res.status(403).send({ auth: false, message: 'No token provided.'});
         else {
             jwt.verify(sessionToken, process.env.JWT_SECRET, (err, decoded) => {
                 if (decoded) {
                     User.findOne({where: { id: decoded.id}}).then(user => {
+                        console.log('***********Inside decoded and findOne');
                         req.user = user;
                         next();
                     },
                     function(){
                         res.status(401).send({error: 'Not authorized'});
+                        console.log('************Error 401');
                     });
                 } else {
                     res.status(400).send({error: 'Not authorized'});
+                    console.log('**********Error 400');
                 }
             });
         }
